60bce2e0865e07e21b209d27d22bc9ef0f2cc1b5
sb0321 <woltaryksb1@gmail.com>
2021-05-02 23:15:19 +0900

    feat: MemberServiceTests 테스트 작성

--- src/test/java/com/assign/organization/persistence/JpaEntityManagerTests.java
+++ src/test/java/com/assign/organization/persistence/JpaEntityManagerTests.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityManagerFactory;
@@ -14,7 +13,7 @@ import javax.persistence.EntityManagerFactory;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
-@DataJpaTest
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class JpaEntityManagerTests {
--- src/test/java/com/assign/organization/service/member/MemberServiceTests.java
+++ src/test/java/com/assign/organization/service/member/MemberServiceTests.java
@@ -1,13 +1,21 @@
 package com.assign.organization.service.member;
 
+import com.assign.organization.domain.member.CSVMemberVO;
+import com.assign.organization.domain.member.Contact;
 import com.assign.organization.domain.member.Member;
-import com.assign.organization.domain.member.repository.MemberRepository;
 import com.assign.organization.domain.member.MemberVO;
+import com.assign.organization.domain.member.repository.MemberRepository;
+import com.assign.organization.domain.team.Team;
+import com.assign.organization.domain.team.repository.TeamRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -21,7 +29,108 @@ class MemberServiceTests {
     private MemberRepository memberRepository;
 
     @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
     private MemberService memberService;
+
+    @Test
+    @Transactional
+    public void testIsertMembersFromCSVMemberVOList() {
+
+        // given
+        List<CSVMemberVO> csvMemberVOList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            CSVMemberVO csvMemberVO = CSVMemberVO
+                    .builder()
+                    .id((long) i)
+                    .name("test" + i)
+                    .teamName("team" + i)
+                    .businessCall("100" + i)
+                    .cellPhone("010-0000-000" + i)
+                    .position("사원")
+                    .duty("팀장")
+                    .build();
+
+            csvMemberVOList.add(csvMemberVO);
+        }
+
+        // when
+        memberService.insertMembersFromCSVMemberVOList(csvMemberVOList);
+
+        // then
+        List<Member> findMemberList = (List<Member>) memberRepository.findAll();
+
+        assertEquals(csvMemberVOList.size(), findMemberList.size());
+
+        log.info(findMemberList.toString());
+
+        for (int i = 0; i < findMemberList.size(); i++) {
+            assertEquals(csvMemberVOList.get(i).getName(), findMemberList.get(i).getName());
+        }
+
+    }
+
+    @Test
+    @Transactional
+    public void testFindMembersContainsKeyword() {
+
+        // given
+        Contact contact = Contact
+                .builder()
+                .businessCall("1001")
+                .cellPhone("010-1234-1111")
+                .build();
+
+        Member member = Member
+                .builder()
+                .id(1L)
+                .name("test")
+                .contact(contact)
+                .duty("팀장")
+                .position("차장")
+                .build();
+
+        Team team = Team
+                .builder()
+                .name("testTeam")
+                .build();
+
+        teamRepository.save(team);
+
+        member.changeTeam(team);
+
+        memberRepository.save(member);
+
+        // when
+        String nameKeyword = "test";
+
+        List<MemberVO> findMembers = memberService.findMembersContainsKeyword(nameKeyword);
+        log.info(findMembers.toString());
+        assertEquals(member.getName(), findMembers.get(0).getName());
+
+        // when
+        String businessCallKeyword = "1001";
+
+        findMembers = memberService.findMembersContainsKeyword(businessCallKeyword);
+        log.info(findMembers.toString());
+        assertEquals(member.getContact().getBusinessCall(), findMembers.get(0).getBusinessCall());
+
+        // when
+        String cellPhoneKeyword = "010-1234-1111";
+
+        findMembers = memberService.findMembersContainsKeyword(cellPhoneKeyword);
+        log.info(findMembers.toString());
+        assertEquals(member.getContact().getCellPhone(), findMembers.get(0).getCellPhone());
+
+        // when
+        String teamNameKeyword = "testTeam";
+
+        findMembers = memberService.findMembersContainsKeyword(teamNameKeyword);
+        log.info(findMembers.toString());
+        assertEquals(member.getTeam().getName(), findMembers.get(0).getTeamName());
+
+    }
 
     @Test
     public void testFindMemberById() {
@@ -34,6 +143,7 @@ class MemberServiceTests {
                 .id(memberId)
                 .name("test")
                 .duty("팀장")
+                .position("차장")
                 .build();
 
         memberRepository.save(member);
@@ -61,8 +171,8 @@ class MemberServiceTests {
                 .name("test")
                 .businessCall("1001")
                 .cellPhone("010-1111-1111")
-               .duty("팀장")
-               .position("사원")
+                .duty("팀장")
+                .position("사원")
                 .build();
 
         // ok
