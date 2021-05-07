5dd7ba242a6cd15a376e8c2bbf2e9cbf5a7e30a7
sb0321 <woltaryksb1@gmail.com>
2021-05-07 16:56:25 +0900

    feat: MainAPIControllerTests, MemberAPIControllerTests 작성

--- src/test/java/com/assign/organization/controller/main/MainAPIControllerTests.java
+++ src/test/java/com/assign/organization/controller/main/MainAPIControllerTests.java
@@ -1,19 +1,46 @@
 package com.assign.organization.controller.main;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
 @SpringBootTest
-@AutoConfigureMockMvc
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource(value = "classpath:application.properties")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MainAPIControllerTests {
 
+
+    MockMvc mockMvc;
+
     @Autowired
-    private MockMvc mockMvc;
+    MainAPIController controller;
+
+    @BeforeAll
+    void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void testSearchKeywordMemberVOList() throws Exception {
+
+        mockMvc.perform(
+                get("/init")
+        )
+        .andDo(print())
+        .andExpect(status().isOk());
+    }
 
 }
--- src/test/java/com/assign/organization/controller/member/MemberAPIControllerTests.java
+++ src/test/java/com/assign/organization/controller/member/MemberAPIControllerTests.java
@@ -1,45 +1,54 @@
 package com.assign.organization.controller.member;
 
-import com.assign.organization.domain.member.CSVMemberVO;
+import com.assign.organization.exception.CSVFileInvalidException;
 import com.assign.organization.service.member.MemberService;
-import com.assign.organization.utils.CSVReader;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
+@Transactional
 @SpringBootTest
-@AutoConfigureMockMvc
 @TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource(value = "classpath:application.properties")
 class MemberAPIControllerTests {
 
-    @Value("${csv.data}")
-    private String csvPath;
-
-    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private MemberService memberService;
+    MemberAPIController controller;
+
+    @Autowired
+    MemberService memberService;
+
+    @Value(value = "${csv.data}")
+    String CSV_FILE_PATH;
+
+    @BeforeAll
+    void init() throws CSVFileInvalidException {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+
+        memberService.insertMembersFromCSVFile(CSV_FILE_PATH);
+    }
 
     @Test
-    @Transactional
-    public void testSearchKeywordMemberVOList() throws Exception {
-
-        memberService.insertMembersFromCSVFile(csvPath);
+    void testSearchKeywordMemberVOList() throws Exception {
 
         MvcResult mvcResult = mockMvc.perform(get("/member")
                 .param("keyword", "승빈"))
