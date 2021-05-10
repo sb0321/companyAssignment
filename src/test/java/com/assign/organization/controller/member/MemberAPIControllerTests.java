45c701cb824f0e0553a05c3b72bbca33afe05bd5
sb0321 <woltaryksb1@gmail.com>
2021-05-10 10:32:29 +0900

    refactor: MemberAPIControllerTests 테스트 수정

--- src/test/java/com/assign/organization/controller/member/MemberAPIControllerTests.java
+++ src/test/java/com/assign/organization/controller/member/MemberAPIControllerTests.java
@@ -1,11 +1,16 @@
 package com.assign.organization.controller.member;
 
+import com.assign.organization.domain.member.MemberVO;
 import com.assign.organization.exception.CSVFileInvalidException;
 import com.assign.organization.service.member.MemberService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +21,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -49,16 +57,22 @@ class MemberAPIControllerTests {
         memberService.insertMembersFromCSVFile(CSV_FILE_PATH);
     }
 
-    @Test
-    void testSearchKeywordMemberVOList() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = {"승빈", "1000", "웹개발 1팀", ""})
+    void testSearchKeywordMemberVOList(String keyword) throws Exception {
 
         MvcResult mvcResult = mockMvc.perform(get("/member")
-                .param("keyword", "승빈"))
+                .param("keyword", keyword))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andReturn();
 
-        log.info(mvcResult.getRequest().getContentAsString());
+        ObjectMapper mapper = new ObjectMapper();
+        List<MemberVO> memberVOList = mapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<MemberVO>>() {});
+
+        List<MemberVO> expected = memberService.findMembersContainsKeyword(keyword);
+
+        assertEquals(expected, memberVOList);
     }
 
 }
